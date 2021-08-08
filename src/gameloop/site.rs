dd16c1a822bb70a66f17d1d15ff8d44e40e3c226
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-08 21:10:08 +0200

    fixup! feat: split site/station instruction

--- src/gameloop/site.rs
+++ src/gameloop/site.rs
@@ -17,18 +17,10 @@ pub fn all(statics: &Statics) -> anyhow::Result<()> {
     let mut some_error = false;
 
     let mut players_in_warp = Vec::new();
-    let mut players_docked = Vec::new();
     for (player, location) in read_all_player_locations() {
         let solarsystem = location.solarsystem();
         match location {
-            PlayerLocation::Site(_) => {}
-            PlayerLocation::Station(station) => {
-                players_docked.push((
-                    player,
-                    solarsystem,
-                    site::Info::generate_station(solarsystem, station.station).site_unique,
-                ));
-            }
+            PlayerLocation::Site(_) | PlayerLocation::Station(_) => {}
             PlayerLocation::Warp(warp) => {
                 players_in_warp.push((player, solarsystem, warp.towards_site_unique));
             }
@@ -41,19 +33,7 @@ pub fn all(statics: &Statics) -> anyhow::Result<()> {
             .filter(|o| o.1 == solarsystem && o.2 == site_info.site_unique)
             .map(|o| o.0.to_string())
             .collect::<Vec<_>>();
-        let players_docked = players_docked
-            .iter()
-            .filter(|o| o.1 == solarsystem && o.2 == site_info.site_unique)
-            .map(|o| o.0.to_string())
-            .collect::<Vec<_>>();
-
-        if let Err(err) = handle(
-            statics,
-            solarsystem,
-            &site_info,
-            &players_warping_in,
-            &players_docked,
-        ) {
+        if let Err(err) = handle(statics, solarsystem, &site_info, &players_warping_in) {
             some_error = true;
             eprintln!("ERROR gameloop::site::handle {}", err);
         }
@@ -70,7 +50,6 @@ fn handle(
     solarsystem: solarsystem::Identifier,
     site_info: &site::Info,
     players_warping_in: &[player::Identifier],
-    players_docked: &[player::Identifier],
 ) -> anyhow::Result<()> {
     let site_unique = &site_info.site_unique;
 
@@ -89,15 +68,14 @@ fn handle(
         players_in_site
             .iter()
             .chain(players_warping_in)
-            .chain(players_docked)
             .collect::<Vec<_>>()
     };
 
     let mut instructions = {
         let mut result = HashMap::new();
-        for player in &all_players_involved {
+        for player in &players_in_site {
             let instructions = read_player_site_instructions(player);
-            result.insert((*player).to_string(), instructions);
+            result.insert(player.to_string(), instructions);
         }
         result
     };
